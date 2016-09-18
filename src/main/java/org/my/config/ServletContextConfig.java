2a94e256ba8fd9ac6273382c876436d9d475422d
Hugh-Kim <nice5340@gmail.com>
2016-09-18 20:26:25 +0900

    add bootstrap lib

--- src/main/java/org/my/config/ServletContextConfig.java
+++ src/main/java/org/my/config/ServletContextConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -16,7 +18,7 @@ import org.springframework.web.servlet.view.JstlView;
         useDefaultFilters = false
         , basePackages = {"org.my.board"}
         , includeFilters = {@ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION)})
-public class ServletContextConfig {
+public class ServletContextConfig extends WebMvcConfigurationSupport {
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -24,5 +26,10 @@ public class ServletContextConfig {
         viewResolver.setPrefix("/WEB-INF/view/jsp/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Override
+    protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
--- src/main/webapp/WEB-INF/view/jsp/articleList.jsp
+++ src/main/webapp/WEB-INF/view/jsp/articleList.jsp
@@ -9,14 +9,31 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
+    <link rel="stylesheet" href="/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+    <script src="/js/jquery-3.1.0.min.js"></script>
+    <script src="/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
     <title>board list page.</title>
 </head>
 <body>
 this is boradlist page.
 
-<c:forEach var="article" items="${articleList}">
-    <p>${article.title}</p>
-</c:forEach>
+<div class="table-responsive">
+    <table class="table table-bordered table-striped table-hover table-condensed">
+        <thead>
+        <th>No.</th>
+        <th>Title</th>
+        </thead>
+        <tbody>
+        <c:forEach var="article" items="${articleList}">
+            <tr>
+                <td>${article.seqNo}</td>
+                <td>${article.title}</td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+</div>
 
 </body>
 </html>
